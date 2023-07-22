1ec6b9438927ea09a3b321a9946b293ecd9da12e
Ryan <ryanpvoong@gmail.com>
2023-07-22 15:34:42 -0400

    refactored functionality to save reddit posts including posting modal

---
+++ src/components/modals/save-post-modal.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Modal, Button } from 'react-bootstrap'
+
+export function PostModal({show, handleClose, submitBookmark, postItem, setPostItem, postData, categories}) {
+  const bookmarkRef = React.useRef("");
+
+  async function handleAddBookmark(value) { 
+    return setPostItem((prev) => {
+      return { ...prev, ...value, ...postData} 
+    })
+  };
+  console.log("postitem: ", postItem)
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Bookmark</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Type in category name.
+          <form onSubmit={submitBookmark}>
+            <label>Category</label>
+            &nbsp;
+            <input id="cat_name" ref={bookmarkRef} list="categoryName" value={postItem.categoryName} 
+            onChange={ (e) => handleAddBookmark({ categoryName: e.target.value })}></input>
+          </form>
+          
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="outline-secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="outline-primary" onClick={submitBookmark}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
--- src/components/views/reddit-posts.js
+++ src/components/views/reddit-posts.js
@@ -2,15 +2,15 @@ import React, { useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/auth-context';
 import { Button, InputGroup, Form } from 'react-bootstrap';
-// import { PostModal } from '../components/Modals/PostModal'
 // import Pagination from "../layouts/Pagination"
 import { useUserContext } from '../../contexts/user-context';
+import { PostModal } from '../modals/save-post-modal';
 
 export default function RedditPosts() {
   const serverUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_DEPLOYED_SERVER : "http://localhost:7979";
   const clientUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_DEPLOYED_CLIENT : "http://localhost:3000";
-  const { savedList, setSavedList } = useUserContext;
-  const clientid = process.env.REACT_APP_REDDIT_ID;
+  const { savedList, setSavedList } = useUserContext();
+  const clientid = process.env.REACT_APP_REDDIT_ID; // Reddit client ID
   const baseUrl = 'https://www.reddit.com'
   const navigate = useNavigate();
   const { currentUser, categories, currentPage, setCurrentPage, setSearchResponse } = useAuth();
@@ -23,8 +23,7 @@ export default function RedditPosts() {
   const [postItem, setPostItem] = React.useState(
     {
     categoryName: "",
-    categoryId: ""
-   })
+   });
   //  const [currentPage, setCurrentPage] = React.useState(1);
    const [fetchedPostsPerPage, setFetchedPostsPerPage] = React.useState(5);
    const indexOfLastPost = currentPage * fetchedPostsPerPage;
@@ -39,13 +38,13 @@ export default function RedditPosts() {
         {/* "name" is redditAPI's "fullname" identifier */}
         <td>{ props.listItem.id }</td>  
         <td>
-          <Link to={{pathname: `${baseUrl}${props.listItem.permalink}`}} target="_blank" onClick={() => alert('opening new tab')}>
+          <Link to={`${baseUrl}${props.listItem.permalink}`} target="_blank" onClick={() => alert('opening new tab')}>
           { props.listItem.title || props.listItem.link_title + " (saved comment)" }
           </Link>
           </td>
           <td>
             {/* <Button onClick={() => btnClick(props.listItem.name) }>+</Button> */}
-            <Button variant="outline-primary" onClick={() => btnClick({ name: props.listItem.name, pathname: props.listItem.permalink, title: props.listItem.title,
+            <Button variant="outline-primary" onClick={() => btnClick({ name: props.listItem.name, pathName: props.listItem.permalink, title: props.listItem.title,
                author: props.listItem.author, subreddit: props.listItem.subreddit_name_prefixed, body: props.listItem.body, link_title: props.listItem.link_title, 
                over_18: props.listItem.over_18 })}
                >+</Button>
@@ -55,7 +54,6 @@ export default function RedditPosts() {
   
 
   // Search ALL saved reddit posts/bookmarks
-
   function filterList(e) {
     e.preventDefault();
     setCurrentPage(1)
@@ -68,7 +66,7 @@ export default function RedditPosts() {
   })
 
     setSearchResponse(savedPostsSearch)
-  }
+  };
 
   // React.useEffect(() => {
   //   async function totalProcess(token) {
@@ -93,78 +91,62 @@ export default function RedditPosts() {
 
   // Retrieve users' saved posts/comments
   async function getRedditPosts() {
-    const fetchRequest = await fetch(`${ serverUrl }/user-reddit/saved-posts`, 
+    const fetchRequest = await fetch(`${ serverUrl }/user-reddit/saved-reddit-posts`, 
     { credentials: "include" }
     )
     .then(res => res)
     .then(data => data.json())
     .catch((error) => console.log(error))
-    
-    // const fetchedData = await fetchRequest.json()
-    await setSavedList(fetchRequest)
-    // console.log("refresh success");
-    // console.log(savedList); // response array
-  }
 
+    setSavedList(fetchRequest)
+  };
 
+  // Display users reddit saved posts onto table
   function displayPosts() {
-    
-    if(savedList) {
+    if(savedList.length > 0) {
       return currentPosts.map((listItem) => {
         return (
-          <FullList listItem={listItem} key={listItem.permalink} bookmarkRef={bookmarkRef}/>
-        )
-      })
+          <FullList listItem={ listItem.data } key={ listItem.data.permalink } bookmarkRef={ bookmarkRef }/>
+        );
+      });
     }
-  }
+  };
 
-  const btnClick = async(value) => { // btn click will store the value into state, which we can then use to pass info to submitBookmark()
+  // btnClick() will store the value into state, which we can then use to pass info to submitBookmark()
+  const btnClick = async(value) => { 
     await setShow(show => !show);
     await setPostData(value);
-    // console.log(value);
-  }
+  };
   
   async function submitBookmark(e) {
     e.preventDefault();
-    
-    const categoryIdName = categories.map(category => category.categoryName.toUpperCase()) // returns category names (categoryName values)
-    const keyId = categories.filter(category => category.categoryName.toUpperCase() === postItem.categoryName.toUpperCase()) // returns the single category that === input
-    // const _id = keyId[0]._id;
-    // accessing the _id through this method since the filter should return an array with only a single obj
-    // can map() toUpperCase to categoryId for case insensitivity *** better for search
-    // console.log(categoryIdName);
-
-    
     try {
-      if (categoryIdName.includes(postItem.categoryName.toUpperCase())) {
-        const categoryFilter = categories?.filter(category => category.categoryName === postItem.categoryName)
-        const catId = categoryFilter.map(el => el._id).toString()
+      const categoryNameCheck = categories.map(category => category.categoryName.toUpperCase()); // returns category names (categoryName values)
+      if (categoryNameCheck.includes(postItem.categoryName.toUpperCase())) {
+        const categoryFilter = categories.filter(category => category.categoryName === postItem.categoryName); // should always return only 1 object
+        // const catId = categoryFilter.map(el => el._id).toString()
 
-      handleClose();
-      setPostItem({
-        categoryName: "",
-        categoryId: ""
-       });
-      // is this good practice accessing the array object?
+        handleClose();
+        setPostItem({
+          categoryName: ""
+        });
+        //next steps, fetch the category and update the list[] field. need to push into array?
 
-
-      //next steps, fetch the category and update the list[] field. need to push into array?
-      // await fetch(`http://localhost:4554/update/${_id}`, {
-      await fetch(`https://saveredd-api.onrender.com/addbookmark`, {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify({...postItem, categoryId: catId})
-      });
-      
-    } else {alert('category does NOT exist OR check spelling - case sensitive')}
-
-  } catch (error) {
-    console.log(error);
-  }
-  }
+        await fetch(`${ serverUrl }/bookmarker/saved-bookmarks`, {
+          method: 'POST',
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ...postItem, categoryId: categoryFilter[0]._id })
+        });
+      } else {
+        alert('category does NOT exist OR check spelling - case sensitive');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
 
 
@@ -201,10 +183,10 @@ export default function RedditPosts() {
             <tr>
               <th style={{ margin: 20, border: "1px solid black", padding: "10px 10px" }}>ID</th>
               <th style={{ margin: 20, border: "1px solid black", padding: "10px 10px" }}>Bookmarks</th>
-              <th style={{ width: 10, border: "1px solid black" }} ><Button variant="link" size="sm" onClick={() => getRedditPosts()}>Load Bookmarks</Button></th>
+              <th style={{ width: 10, border: "1px solid black" }} ><Button variant="link" size="sm" onClick={ () => getRedditPosts() }>Load Bookmarks</Button></th>
             </tr>
         </thead>
-        {/* <tbody>{displayPosts()}</tbody> */}
+        <tbody>{ displayPosts() }</tbody>
       </table>
     </div>
 
@@ -220,10 +202,11 @@ export default function RedditPosts() {
     <datalist id='categoryName'>
       { categories?.map(results => { return (<option key={results._id}>{results.categoryName}</option> ) }) }
     </datalist>
-    {/* <div>
-  <PostModal show={show} handleClose={handleClose} list={"categoryName"} categories={categories}
-  submitBookmark={submitBookmark} postItem={postItem} setPostItem={setPostItem} postData={postData} />
-    </div> */}
+
+    <div>
+      <PostModal show={show} handleClose={handleClose} list={"categoryName"} categories={categories}
+      submitBookmark={submitBookmark} postItem={postItem} setPostItem={setPostItem} postData={postData} />
+    </div>
 
     {/* *****ENSURE REDDIT TOKEN REVOKED AND STATE REMOVED***** */}
     {/* do a check if data has been rendered, if not then button shows. also, if no saved bookmarks, then return a string saying so  */}
