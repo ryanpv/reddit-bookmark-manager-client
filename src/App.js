b9d99488c28d5a7fec28dbf0d671d944d7b7d940
Ryan <ryanpvoong@gmail.com>
2023-07-25 17:08:54 -0400

    removed some unused code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { useLocation, Routes, Route } from 'react-router-dom';
-import { auth } from "./firebase";
+import { Routes, Route } from 'react-router-dom';
 import AppNavbar from "./components/navbars/navbar.js";
 import { useAuth } from "./contexts/auth-context.js";
-import { UserProvider } from "./contexts/user-context";
 import Sidebar from "./components/sidebar/sidebar.js";
-import sidebarRoutes from "./sidebar-routes";
 import Signup from "./components/account/signup";
 import Login from "./components/account/login";
 import ResetPassword from "./components/account/reset-password";
@@ -16,15 +13,7 @@ import CategoryContent from "./components/views/category-content";
 
 
 function App() {
-  const [image, setImage] = React.useState();
-  const [color, setColor] = React.useState("blue");
-  const [hasImage, setHasImage] = React.useState(true);
-  const location = useLocation();
-  const mainPanel = React.useRef(null);
-  const [token, setToken] = React.useState('')
-  const [loading, setLoading] = React.useState(true)
-  const [tokauth, setTokAuth] = React.useState(false || window.localStorage.getItem('tokauth') === 'true')
-  const { categories, setCategories, categoryIdData, currentUser } = useAuth();
+  const { currentUser } = useAuth();
 
   return (
     <>
@@ -32,8 +21,6 @@ function App() {
       <header className="py-3 mb-4 border-bottom shadow">
         <AppNavbar />
       </header>
-          {/* <div className="container-fluid align-items-center d-flex"> */}
-      {/* <div className="container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto"> */}
           <div className="row flex-grow-sm-1 flex-grow-0">
         <Sidebar />
           <div className="col overflow-auto h-100">
@@ -42,7 +29,7 @@ function App() {
                     <Route path="/user-reddit-posts" element={ <RedditPosts /> } />
                     <Route path="/log_callback" element={ <LogCallback /> } />
                     <Route path="/search-results" element={ <RedditSearchResults /> } />
-                    <Route path="/login" element={ <Login /> } />
+                    { <Route path="/login" element={ <Login /> } /> }
                     <Route path="/sign-up" element={ <Signup /> } />
                     <Route path="/forgot-password" element={ <ResetPassword /> } />
                     { currentUser !== "" ? <Route path="/user/category/:params" element={ <CategoryContent /> } /> : null }
@@ -52,8 +39,6 @@ function App() {
               </div>
           </div>
           </div>
-      {/* </div> */}
-          {/* </div> */}
 
     </>
   );
--- src/components/account/login.js
+++ src/components/account/login.js
@@ -1,12 +1,12 @@
 import React, { useRef } from 'react'
-import { Button, Card, Container, Form, Alert } from 'react-bootstrap'
-import { Link, Navigate } from 'react-router-dom';
+import { Button, Card, Container, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 import { useAuth } from "../../contexts/auth-context.js"
 
 export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const { login, loginWithGoogle, error, setError, currentUser, setUserEmailStore } = useAuth();
+  const { login, loginWithGoogle, setError, currentUser, setUserEmailStore } = useAuth();
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -24,47 +24,48 @@ export default function Login() {
     })
   }
 
+
   return (
     <>
-    { currentUser !== 'undefined' ? 
-    <>
-      <Container className="mt-4" style={{ maxWidth: '325px' }}>
-        <Card>
-          <Card.Body>
-            <h2 className='text-center mb-4'>Log In</h2>
-            {/* { error && <Alert variant='danger'>{error}</Alert> } */}
+    { currentUser === 'undefined' || currentUser === "" ? 
+      <>
+        <Container className="mt-4" style={{ maxWidth: '325px' }}>
+          <Card>
+            <Card.Body>
+              <h2 className='text-center mb-4'>Log In</h2>
+              {/* { error && <Alert variant='danger'>{error}</Alert> } */}
 
-            <Form onSubmit={handleSubmit}>
-              <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" onChange={ (e) => userEmailRef({ setUserEmailStore: e.target.value }) } ref={emailRef} required />
-              </Form.Group>
-              <Form.Group id="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" ref={passwordRef} required />
-              </Form.Group>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" onChange={ (e) => userEmailRef({ setUserEmailStore: e.target.value }) } ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
 
-              <Button variant="outline-primary" className='w-100 mt-4' type='submit'>Log In.</Button>
-            </Form>
-          <hr></hr>
-          </Card.Body>
-            <div className='mb-2 text-center'>
-              <Button variant='link' size='sm' onClick={ loginWithGoogle }>
-                <img src={require('./google-signin-logo.png')} alt='google sign-in logo' width={150} />
-              </Button>
-            </div>
-        </Card>
+                <Button variant="outline-primary" className='w-100 mt-4' type='submit'>Log In.</Button>
+              </Form>
+            <hr></hr>
+            </Card.Body>
+              <div className='mb-2 text-center'>
+                <Button variant='link' size='sm' onClick={ loginWithGoogle }>
+                  <img src={require('./google-signin-logo.png')} alt='google sign-in logo' width={150} />
+                </Button>
+              </div>
+          </Card>
 
-            <div className='w-100 text-center mt-4'>
-              <Link to='/forgot-password'>Forgot Password?</Link>
-            </div>
-      </Container>
+              <div className='w-100 text-center mt-4'>
+                <Link to='/forgot-password'>Forgot Password?</Link>
+              </div>
+        </Container>
 
-      <div className='text-center mt-3'>
-          <p>Don't have an account? Go to the <Link to='/sign-up'>Sign Up</Link> page</p>
-      </div>
-    </>
-   : <Navigate replace to='/' /> }
-</>
+        <div className='text-center mt-3'>
+            <p>Don't have an account? Go to the <Link to='/sign-up'>Sign Up</Link> page</p>
+        </div>
+      </>
+    : <h2>Logged in as { currentUser }</h2> }
+  </>
   )
 }
